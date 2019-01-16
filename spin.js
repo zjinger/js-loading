9a25755fcef599ef519336bd545574e91dd2fe6c
zjinger <892295834@qq.com>
2019-01-16 14:10:53 +0800

    第一次提交
    spin加载组件

---
+++ spin.js
@@ -0,0 +1,53 @@
+; (function (undefined) {
+    // 对象合并
+    function extend(o, n, override) {
+        for (var key in n) {
+            if (n.hasOwnProperty(key) && (!o.hasOwnProperty(key) || override)) {
+                o[key] = n[key];
+            }
+        }
+        return o;
+    }
+    function SpinLoading(elem, opt) {
+        this._initial(elem, opt);
+    }
+    SpinLoading.prototype = {
+        constructor: this,
+        _initial: function (elem, opt) {
+            // 默认参数
+            var def = {
+                text: '加载中...'
+            };
+            if (!opt) opt = {};
+            this.def = extend(def, opt, true); //配置参数
+            this.spinContainer = this._generateTemplate(elem); //存放在实例中的节点
+        },
+        _generateTemplate: function (elem) {
+            var hostDiv = document.getElementById(elem);
+            hostDiv.classList.add('ant-spin-nested-loading');
+            var docContainer = document.createElement('div'); // 空的div
+            var _text = this.def.text ? '<div class="ant-spin-text">' + this.def.text + '</div>' : ''
+            var _innerHTML = '<div class="ant-spin ant-spin-show-text ant-spin-spinning"><span class="ant-spin-dot ant-spin-dot-spin"><i></i><i></i><i></i><i></i></span>'
+                + _text
+                + '</div>';
+            docContainer.innerHTML = _innerHTML;
+            hostDiv.appendChild(docContainer);
+            return docContainer;//控制这个div 去close 与show spin
+        },
+        show: function () {
+            this.spinContainer.style.display = 'block';
+        },
+        close: function () {
+            this.spinContainer.style.display = 'none';
+        }
+    }
+    // 最后将插件对象暴露给全局对象
+    _global = (function () { return this || (0, eval)('this'); }());
+    if (typeof module !== "undefined" && module.exports) {
+        module.exports = SpinLoading;
+    } else if (typeof define === "function" && define.amd) {
+        define(function () { return SpinLoading; });
+    } else {
+        !('SpinLoading' in _global) && (_global.SpinLoading = SpinLoading);
+    }
+}());
